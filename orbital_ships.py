03bccc9743a90d06f3aa41822942d62fe7256369
Steve Evans <schevans@github.com>
2024-11-17 18:13:05 +0000

    Missed svn add.
    git-svn-id: file:///home/steve/svn/G3@13 2c4f788c-be06-4e6c-9a5a-78778216f3cc

---
+++ orbital_ships.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 14 18:58:26 2024
+
+@author: steve
+"""
+import math 
+from pygame.math import Vector2
+
+import ships        
+import constants as const
+
+G = 1
+
+class OrbitalShip(ships.Ship):
+    
+    def __init__(self, ship, planet, r, p ):
+        
+        
+        self.__dict__.update(vars(ship))
+        
+       
+        self.r = r
+        self.p = p
+        self.acceleration = 0
+        
+        self.xy = Vector2(const.screen_center.x - math.cos(self.p)*self.r,  const.screen_center.y - math.sin(self.p)*self.r)
+        
+        self.mass = 7000 / planet.size # FIXME
+        print(self.mass)
+        self.w = math.sqrt(G*self.mass/math.pow(r, 3)) 
+
+        
+    def update(self):
+        
+        if self.acceleration:
+            self.r += self.acceleration
+            self.image = self.image_flying
+        else:
+            self.image = self.image_still 
+        
+        if self.acceleration < 0:
+            self.image.update(self.xy, 180)
+            
+        self.p += self.w
+        self.w = math.sqrt(G*self.mass/math.pow(self.r, 3)) 
+        
+        self.heading = math.degrees(-self.p)
+        self.xy = Vector2(const.screen_center.x - math.cos(self.p)*self.r,  const.screen_center.y - math.sin(self.p)*self.r)
+  
+        reverse = 0
+        if self.acceleration < 0:
+            reverse = 180
+            
+        self.image.update(self.xy, self.heading+reverse)
+    
+        
+
+            #self.image = pygame.transform.rotate(self.image, 180)
+
+            
+            
+            
