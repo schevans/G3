836db87bf60a978c7773eba64a2c8eb1a7d25650
Steve Evans <schevans@github.com>
2025-03-03 20:10:14 +0000

    Start of cleanup of planetary_textures.
    git-svn-id: file:///home/steve/svn/G3@166 2c4f788c-be06-4e6c-9a5a-78778216f3cc

--- planetary_textures.py
+++ planetary_textures.py
@@ -12,10 +12,12 @@ import numpy as np
 import random
 
 import utils
+import constants as const
+from rotatable_image import RotatableImage
 
 WIDTH = 1200
 HEIGHT = 800
-CENTER = [WIDTH/2, HEIGHT/2]
+
 
 
 pygame.init()
@@ -25,17 +27,12 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
 
 
-FEATURE_SIZE = 40
 
 size = 200
-r = 100
-S = r
+
 x0 = 50
 y0 = 50
-z0 = 50
-#rng = numpy.random.default_rng(seed=0)
 
-#ix, iy, iz= rng.random(size), rng.random(size), rng.random(size),
 
 # lava
 color1 = pygame.Color('yellow')
@@ -81,7 +78,7 @@ def print_thing(array, x_offset, y_offset=50):
             
             screen.set_at((x+x_offset, y+y_offset), color)
             
-#value = opensimplex.noise3array(ix, iy, iz)
+
 
 def clamp(n, smallest, largest): return max(smallest, min(n, largest))
 def butterfly(n, smallest, largest): return max(largest, min(n, smallest))
@@ -102,16 +99,12 @@ clamp_high = clamp_low + 0.1 # (random.random() +0.5 ) * 2 # random.random()  # 
 if clamp_low > clamp_high:
     clamp_low, clamp_high = clamp_high, clamp_low 
 
-print(clamp_low, clamp_high)
+
 
 
 for y in range(0, size):
     for x in range(0, size):
             
-        #r = math.sqrt(math.pow(x-x0, 2) + math.pow(y-y0, 2) + math.pow(z-z0, 2))
-
-        #if r == 50:
-        #if math.isclose(r, 50, abs_tol=2):
         A[x,y] = opensimplex.noise2(x/FEATURE_SIZE,y/FEATURE_SIZE)
         M[x,y] = opensimplex.noise2(x/LARGE,y/LARGE)   
 
@@ -120,28 +113,12 @@ for y in range(0, size):
         R = 2; # Higher for less mountains.
         A[x,y] = pow(A[x,y],R);
 
-        #E2[x,y] = M[x,y] * A[x,y]
         P = 2   # Higher for stronger bias.  
         E2[x,y] = M[x,y] * pow(A[x,y]*2,P)/2 if A[x,y]  < 0 else 1-(pow((1-A[x,y])*2,P)/2)
-        #E = M*(A<0.5?power(A*2,P)/2:1-(power((1-A)*2,P)/2));
 
         E3[x,y] = clamp(E2[x,y], clamp_low, clamp_high)
 
-        #warpstrength = 1
-        #dx = warpstrength * opensimplex.noise2(x+123, y+456)
-        #dy = warpstrength * opensimplex.noise2(x-789, y-456)
-        #E3[x,y] = opensimplex.noise2( x + dx, y + dy);
 
-
-
-        #value = (value1 + value2 ) /2 
-
-        #color = (0, int((value + 1) * 128), 0)
-        #screen.set_at((x+x0, y+y0), color)
-
-
-                    
-                #screen.set_at((x+x0, y+y0), 'green')
 
 #np.save('./moo.txt', A)
 
@@ -162,24 +139,21 @@ def get_image(planet):
     
     planet_view_r = planet.size * PLANET_VIEW_RADIUS_MULT * 2
     
-    surface1 = pygame.Surface((planet_view_r, planet_view_r), pygame.SRCALPHA)
-    surface2 = pygame.Surface((planet_view_r, planet_view_r), pygame.SRCALPHA)
-    surface3 = pygame.Surface((planet_view_r, planet_view_r), pygame.SRCALPHA)
+    surface = pygame.Surface((planet_view_r, planet_view_r), pygame.SRCALPHA)
     
     for x in range(planet_view_r):
         for y in range(planet_view_r):
-            print(x,y)
-            
+
+            # normalize
             px = x * 2/planet_view_r - 1
             py = y * 2/planet_view_r - 1
             
+            # get the squared magnitude
             magSq = px * px + py * py
             
+            # outside the circle? leave blank.
             if ( magSq > 1 ):
                 continue
-            
-            pxo = px
-            pyo = py
             
             # lens distortion
             scale = 0.35 * magSq + (1 - 0.35)
@@ -187,20 +161,11 @@ def get_image(planet):
             py = py * scale
             
             # convert our local offsets into lookup coordinates into our map texture
-            u = time * rotationSpeed + (px + 1) * (mapHeight/2) 
+            u = (px + 1) * (mapHeight/2) 
             v = (py + 1) * (mapHeight/2)
-            # wrap the horizontal coordinate around our map when it goes off the edge
-            u = u % (2 * mapHeight)
-            
-            # convert our local offsets into lookup coordinates into our map texture
-            uo = time * rotationSpeed + (pxo + 1) * (mapHeight/2) 
-            vo = (pyo + 1) * (mapHeight/2)
-            # wrap the horizontal coordinate around our map when it goes off the edge
-            uo = uo % (2 * mapHeight)
+
             
             moo = E1[int(u)][int(v)]
-            color1 = (0, (int(E1[int(u)][int(v)] + 1) * 128), 0)
-            coloro = (0, (int(E1[int(uo)][int(vo)] + 1) * 128), 0)
         
             moo = ( moo + 1 ) / 2 # normal
             t1 = 207
@@ -209,17 +174,12 @@ def get_image(planet):
         
             newcolor = ( t1, t2, t3 )
         
-        
-                
-            #print(newcolor)
-            surface1.set_at((x, y), pygame.Color('white'))
-            surface2.set_at((x, y), newcolor)
-            surface3.set_at((x, y), coloro)
-         
-        #surface2.fill('blue')
+
+            surface.set_at((x, y), newcolor)
+
 
             
-    return surface2
+    return surface
 
 class Planet():
     
@@ -228,6 +188,11 @@ class Planet():
         self.size = size
 
 planet = Planet(10)
+surface = get_image(planet)
+
+rot_image = RotatableImage(const.screen_center, surface)
+
+spin = 0
 
 while True:
     
@@ -237,10 +202,13 @@ while True:
             raise SystemExit
         
     surface = get_image(planet)
-    screen.blit(surface, (500, 500))
     
-    time += 1
-    #spin += 1
+    
+    rot_image.angle_deg = spin
+    
+    rot_image.draw(screen)
+    
+    spin += 1
     
     pygame.display.flip()  # Refresh on-screen display
     clock.tick(24)        
@@ -248,78 +216,7 @@ while True:
 
 
 
-          
-"""
-
-print('Generating 2D image...')
-
-rng = numpy.random.default_rng(seed=0)
-ix, iy = rng.random(100), rng.random(100)
-opensimplex.noise2array(ix, iy)
-
-print('done1')
-
-pos = 0
-neg = 0
-
-FEATURE_SIZE = 5
-
-size = 300
-r = 100
-S = r
-for y in range(0, size):
-    for x in range(0, size):
-        center = (size/2, size/2)
-        
-        rx = math.sqrt((x-center[0])**2 + (y-center[1])**2) / r
-        
-        rx = rx *2
-        
-        tmp = 0.5
-        
-        if rx != 0:
-            
-            value = opensimplex.noise2((x / (FEATURE_SIZE/rx))+tmp, (y / (FEATURE_SIZE/rx)+tmp))
-            #value = opensimplex.noise2(2*x / FEATURE_SIZE, y / FEATURE_SIZE)
-            color = (0, int((value + 1) * 128), 0)
-            screen.set_at((x, y), color)
-            
-            longitude = math.radians(x)
-            latitude = math.radians(y)
-            
-            #r = sqrt( x^2 + y^2 + z^2 )
-            #theta = arctan( y/x )
-            #phi = arccos( z/r )
-            # treat theta as longitude and phi as latitude
-            
-            
-            #longitude = x / r
-            #latitude = 2 * math.atan(math.exp(y/r)) - math.pi/2
-            
-            Px = S * math.cos(latitude) * math.cos(longitude)
-            Py = S * math.cos(latitude) * math.sin(longitude)
-            Pz = S * math.sin(latitude)
-        
-            screen.set_at((x, y), color)
-            
-            screen.set_at((400+round(Px), 200+round(Py)), color)
-        
-            if Pz > 0:
-                pos += 1
-            elif Pz <= 0:
-                neg += 1
-            
-        
-        
-        
-print('done2', pos, neg)
 
 
 
-
-
-
-    
-    
-"""  
     
